8ac0fc5d1e61bd5c1c91078fd3e0162d23a53d09
Ruslan Soloviev <ruslansoloviev@yandex.ru>
2026-02-15 16:28:43 +0300

    Set logging level for httpcore

--- backend/app/__main__.py
+++ backend/app/__main__.py
@@ -20,5 +20,6 @@ handler.setFormatter(ColoredFormatter(
 ))
 logging.basicConfig(level=logging.DEBUG, handlers=[handler])
 logging.getLogger('telethon').setLevel(logging.INFO)
+logging.getLogger('httpcore').setLevel(logging.INFO)
 
 uvicorn.run(app, host='0.0.0.0', port=8000)
